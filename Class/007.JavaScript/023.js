c532158771b151fa4aa2dd82c79695e275931e6d
chaeng <cathyshim222@naver.com>
2021-12-09 09:27:35 +0900

    폴더 이동

---
+++ Class/007.JavaScript/023.js
@@ -0,0 +1,15 @@
+fetch('https://raw.githubusercontent.com/paullabkorea/coronaVaccinationStatus/main/data/data.json')
+    .then(function(response) {
+        console.log(1);
+        return response.json();
+    })
+    .then(function(json) {
+        console.log(2);
+        console.log(json);
+        return json
+    })
+    .then(function(json) {
+        console.log(3);
+        console.log(json.filter(s => s['시·도별(1)'] === '전국'));
+        return
+    })
---
+++ Class/손코딩/손코딩2탄-1.js
@@ -0,0 +1,15 @@
+fetch('https://블라블라.json')
+    .then(function(response) {
+        console.log(1);
+        return response.json();
+    })
+    .then(function(json) {
+        console.log(2);
+        console.log(json);
+        return json
+    })
+    .then(function(json) {
+        console.log(3);
+        console.log(json.filter(s => s['시·도별(1)'] === '전국').map(obj => obj['1차 접종 퍼센트']));
+        return
+    })
